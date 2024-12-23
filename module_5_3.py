5d3f09669381911dc11a1b28291bca5d8b304848
seelk <sedova08111984@gmail.com>
2024-12-23 20:46:57 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -1,66 +1,63 @@
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.nFloors = number_of_floors
-    def go_to(self, new_floor: int):
-        if new_floor > self.nFloors:
-            print('Такого этажа не существует!')
-        else:
-            if new_floor < 1:
-                for f in range(1, new_floor-1, -1):
-                    print('Спускаемся:', f)
-            for f in range(1, new_floor+1):
-                print('Поднимаемся:', f)
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 < new_floor < self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+            else:
+                print('Такого этажа не существует')
 
     def __len__(self):
-        return self.nFloors
+        return self.number_of_floors
+
     def __str__(self):
-        return f'Название: "{self.name}", кол-во этажей: {self.nFloors}.'
+        return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
+
     def __eq__(self, other):
-        return self.nFloors == other.nFloors
+        return self.number_of_floors == other.number_of_floors
     def __lt__(self, other):
-        #(<)
-        return self.nFloors < other.nFloors
+        return self.number_of_floors < other.number_of_floors
     def __le__(self, other):
-        #(<=)
-        return self.nFloors <= other.nFloors
+        return self.number_of_floors <= other.number_of_floors
     def __gt__(self, other):
-        #(>)
-        return self.nFloors > other.nFloors
+        return self.number_of_floors > other.number_of_floors
     def __ge__(self, other):
-        #(>=)
-        return self.nFloors >= other.nFloors
+        return self.number_of_floors >= other.number_of_floors
     def __ne__(self, other):
-        #(!=)
-        return self.nFloors != other.nFloors
+        return self.number_of_floors != other.number_of_floors
     def __add__(self, value):
-        self.nFloors += value
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+    def __radd__(self, value):
+        self.number_of_floors += value
         return self
-    def __iadd__(self, other):
-        return self + other
-    def __radd__(self, other):
-        return self + other
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
 
-def main():
-    h1 = House('ЖК Эльбрус', 10)
-    h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(h1 == h2)
 
-    print(h1)
-    print(h2)
-    print(h1 == h2)  # __eq__
-    h1 = h1 + 10  # __add__
-    print(h1)
-    print(h1 == h2)
-    h1 += 10  # __iadd__
-    print(h1)
-    h2 = 10 + h2  # __radd__
-    print(h2)
-    print(h1 > h2)  # __gt__
-    print(h1 >= h2)  # __ge__
-    print(h1 < h2)  # __lt__
-    print(h1 <= h2)  # __le__
-    print(h1 != h2)  # __ne__
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
 
-if __name__ == '__main__':
-    main()
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
